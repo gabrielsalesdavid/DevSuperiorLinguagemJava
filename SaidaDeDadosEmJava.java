a889f83fa081182cc431d173d002ce5450a0b694
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-12 16:02:27 -0300

    Saida de dados em Java. É necessario que seja declarado os method e as class  em Java para que seja feita a execução do codigo.
    O sinal de soma(+) em Java significa concatenação com as variaveis.
    Já o sinal de igauldade(=) simples. É um parametro que a variavel recebe um valor ou uma outra variavel.
    
    Os tipos das variaveis, são:
    INT. == é do tipo inteiro.
    DOUBLE. == é do tipo de numeros com casas decimais.
    STRING. == é do tipo caracter com frases e espaços em branco, entre aspas dupla.
    CHAR. == é do tipo caracter simples, com apenas uma letra entre aspas simples
    
    public class == seria a classe publica e ou a global.
    public static void main(String[] args) == é um metodo estatico(Static) publico que retorna um argumento(args) do tipo String(Caracter) vazio(void)
    
    Saida de dados, com:
    System.out.println("A mensagem que deseja colocar" + concatenação com o nome da variaveis);
    "LN" é a quebra de linha depois da saida de dados.

--- SaidaDeDadosEmJava.java
+++ SaidaDeDadosEmJava.java
@@ -2,7 +2,17 @@ public class SaidaDeDadosEmJava {
 
     public static void main (String[] args) {
 
-        System.out.print("Bom dia!");
-        System.out.println("Boa noite!");
+        int idade;
+        double salario;
+        String nome;
+        char sexo;
+
+        idade = 31;
+        salario = 4000.0;
+        nome = "Gabriel Sales David";
+        sexo = 'I';
+
+        System.out.println("O(A) funcionario(a) " + nome + ", sexo " + sexo + ", ganha "
+                           + String.format("%.2f", salario) + " e tem " + idade + " anos");
     }
 }
