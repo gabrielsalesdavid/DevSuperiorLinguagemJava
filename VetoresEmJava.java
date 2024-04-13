2882deec45756ce789eb5d0414b6d9058e1aabba
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-12 21:31:55 -0300

    Vetor com for em Java. É necessario que seja declarado os method e as class em Java para que seja feita a execução do codigo.
    A variavel vet, ela pode ser uma String, char, int, double e float. Variavel[] com colchete segnifica que ela é um vetor. Double[] vet = new double[n]; O nome da variavel double, que é o vet, ele recebe(=) o new tipo da variavel passando o valor entre colchete a variavel declarada anteriormente. E dentro do for, o nome da varaivel com os colchetes passa o valor que sera recebido na posição i do for!
    
    FOR, seria um laço de repetição que sabemos quando ele para.
    for (inicialização; condição; incremento). No laço de repetição a inicialização sempre começará em zero(0).
    Condição: se a variavel i for menor(<) ou maior(>) do que a outra variavel, então executa o comando. Do contrario pula do laço de repetição.
    Incremento: vai passando a variavel i para mais um, ate que chegue no valor que a outra variavel possui. Ou seja, o i começa com o valor zero, isto na inicialização. Ai compara: i(zero) é menor do que esta variavel, caso seja, esxecute. Ate que i seja igual ou menor do que o valor daquela varaivel comparada.
    
    A variavel soma é declarada com o valor zero(o). Pois ela sera um comando dentro do for. Sendo assim, a soma recebe ele mesma com concatenando uma outra variavel. Ou seja, a soma valendo zero recebe ela mais a outra variavel.
    
    O sinal de soma(+) em Java significa concatenação com as variaveis.
    Já o sinal de igauldade(=) simples. É um parametro que a variavel recebe um valor ou uma outra variavel.
    
    Tipo da variavel, são:
    INT. == é do tipo inteiro.
    
    public class == seria a classe publica e ou a global.
    public static void main(String[] args) == é um metodo estatico(Static) publico que retorna um argumento(args) do tipo String(Caracter) vazio(void)
    
    Saida de dados, com:
    System.out.println("A mensagem que deseja colocar" + concatenação com o nome da variaveis);
    "LN" é a quebra de linha depois da saida de dados.
    
    Entrada de dados, com:
    sc.nextInt(); == é o scann de dados que o user digita. sc.nextInt é do tipo inteiro. Assim como: nextDouble é do tipo casas decimais, next().charAt(0) é do tipo char com caracter simples, qeu diz ao Java: "Olha voce recebera um unico caracter. Porem, ele pode ser tanto numerico quanto caracter" e o valor 0 entre parentes é um indice que o unico caracter de entrada e nextLine é do tipo de caracter com espaço em branco...

---
+++ VetoresEmJava.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VetoresEmJava {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        System.out.println("Quantos numeros voce vai digitar? ");
+        n = sc.nextInt();
+
+        double[] vet = new double[n];
+
+        for (int i = 0; i < n; i++) {
+           System.out.println("Digite um numero: ");
+           vet[i] = sc.nextDouble();
+        }
+
+        System.out.println("Numeros digitados: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(String.format("%.1f", vet[i]));
+        }
+
+        sc.close();
+    }
+}
