b80b8a6c1d3213065600f76888d78ab09032de55
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-14 11:03:06 -0300

    Add Locale.US...

--- ProblemaIdade.java
+++ ProblemaIdade.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ProblemaIdade {
 
     public static void main () {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
         int idade01, idade02;
