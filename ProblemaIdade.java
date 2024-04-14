f9eddc79232074126e3913fad2a4fbd5ac174617
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-14 10:55:33 -0300

    Vetor com for em Java. É necessario que seja declarado os method e as class em Java para que seja feita a execução do codigo.
    O sinal de soma(+) em Java significa concatenação com as variaveis.
    Já o sinal de igauldade(=) simples. É um parametro que a variavel recebe um valor ou uma outra variavel.
    
    Tipo da variavel, são:
    INT. == é do tipo inteiro.
    DOUBLE. == é do tipo com casas decimais.
    
    public class == seria a classe publica e ou a global.
    public static void main(String[] args) == é um metodo estatico(Static) publico que retorna um argumento(args) do tipo String(Caracter) vazio(void)
    
    Saida de dados, com:
    System.out.println("A mensagem que deseja colocar" + concatenação com o nome da variaveis);
    "LN" é a quebra de linha depois da saida de dados.
    String.format("%.4f", variavel); A String concatenado na saida de dados é um tipo de formatação em Java. Ele formatara a saida conforme for declarada ou parametrizada entre os parentes.
    Já "%.4f" é a formação mostrando ao Java que aquela saida de dados é do tipo double. ".4f" seria a quantidades de casas decimais e "f" seria a simbologia de double/float!
    
    Entrada de dados, com:
    sc.nextInt(); == é o scann de dados que o user digita. sc.nextInt é do tipo inteiro.
    Assim como: nextDouble é do tipo casas decimais, next().charAt(0) é do tipo char com caracter simples, qeu diz ao Java: "Olha voce recebera um unico caracter.
    Porem, ele pode ser tanto numerico quanto caracter" e o valor 0 entre parentes é um indice que o unico caracter de entrada e nextLine é do tipo de caracter com espaço em branco...

---
+++ ProblemaIdade.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ProblemaIdade {
+
+    public static void main () {
+
+        Scanner sc = new Scanner(System.in);
+
+        int idade01, idade02;
+        String nome01, nome02;
+        double media;
+
+        System.out.println("Dados da primeira pessoa: ");
+        System.out.print("Nome: ");
+        nome01 = sc.nextLine();
+        System.out.print("Idade: ");
+        idade01 = sc.nextInt();
+        System.out.println("Dados da segunda pessoa: ");
+        System.out.print("Nome: ");
+        nome02 = sc.nextLine();
+        System.out.print("Idade: ");
+        idade02 = sc.nextInt();
+
+        media = (idade01 + idade02) / 2.0;
+
+        System.out.println("A idade media de " + nome01 + " e " + nome02 + " eh de "
+                                          + String.format("%.1f", media) + " anos");
+
+        sc.close();
+    }
+}
